fa0707165726ef2fb242904f5b3e829b79845de3
Ginie Digital <giniedigitaldeveloper@gmail.com>
2024-06-13 07:43:07 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+const server = http.createServer(app);
+const PORT = process.env.PORT || 3000;
+
+require("dotenv").config();
+
+app.use(morgan("tiny"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => res.status(200).json({ status: "Everything is fine." }));
+
+
+  mongoose.set("strictQuery", true)                                                        
+  mongoose.connect(process.env.MONGO_URL, () => console.log("✔ connected to mongo")    )
+
+
+server.listen(PORT, (err) =>
+  !err ? console.log(`✔ Node Listening to http://localhost:${PORT}`) : console.log("There was some error ", err.message)
+);
